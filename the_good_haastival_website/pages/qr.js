1842f15795cd567f71b6c74003320d6fc8291d8b
Nightfly <nightfly2013@gmail.com>
2021-04-17 15:01:44 +0200

    added new qr code implementation

--- the_good_haastival_website/pages/qr.js
+++ the_good_haastival_website/pages/qr.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import QRReader from "../components/QRReader.tsx";
 
 export default function Home() {
   return (
@@ -9,8 +10,8 @@ export default function Home() {
         <title>Haastival App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
+      <QRReader />
         <h1 className={styles.title}>
         PENIS PENIS PENIS PENIS
         </h1>
