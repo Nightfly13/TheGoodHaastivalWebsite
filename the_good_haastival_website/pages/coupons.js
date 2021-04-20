6110873e9810998ba74caae8f567914c333abb24
Nightfly <nightfly2013@gmail.com>
2021-04-20 15:02:19 +0200

    Added counters to coupons page

--- the_good_haastival_website/pages/coupons.js
+++ the_good_haastival_website/pages/coupons.js
@@ -1,28 +1,84 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import React, { Component } from "react";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Haastival App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+class Page extends Component {
+  state = { largeCounter: 0, smallCounter: 0, allTickets: 0};
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-        PENIS PENIS PENIS PENIS
-        </h1>
-        <div className={styles.grid}>
-        </div>
-        <div class="navbar" id="myNavbar">
-          <Link href={'/qr'}>QR</Link>
-          <Link href={'/coupons'}>Coupons</Link>
-          <Link href={'/refunds'}>Refunds</Link>
-          <Link href={'/pictures'}>Pics</Link>
-        </div>
-      </main>
-    </div>
-  )
+  mealCount = (e) => {
+    console.log(e);
+    if (e.target.id.includes("Large")) {
+      this.setState(({ largeCounter }) => ({
+        largeCounter: e.target.id.includes("inc")
+          ? largeCounter + 1
+          : Math.max(largeCounter - 1, 0),
+      }));
+      this.setState(({allTickets})=> ({allTickets : e.target.id.includes("inc") ? allTickets +1 :  Math.max(allTickets -1)}))
+    } else {
+      this.setState(({ smallCounter }) => ({
+        smallCounter: e.target.id.includes("inc")
+          ? smallCounter + 1
+          : Math.max(smallCounter - 1, 0),
+      }));
+      this.setState(({allTickets})=> ({allTickets : e.target.id.includes("inc") ? allTickets +1 :  Math.max(allTickets -1)}))
+    }
+  };
+  //counter: e.name.inludes ? counter + 1 : counter-1
+  render() {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Haastival App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main className={styles.main}>
+          <h1 className={styles.title}>Tickets: []</h1>
+          <div>
+          <button id="subLarge" onClick={this.mealCount}>
+            -
+          </button>
+          <p id="largeMeal">{this.state.largeCounter}</p>
+          <button id="incLarge" onClick={this.mealCount}>
+            +
+          </button>
+          </div >
+          <div>
+          <button id="subSmall" onClick={this.mealCount}>
+            -
+          </button>
+          <p id="smallMeal">{this.state.smallCounter}</p>
+          <button id="incSmall" onClick={this.mealCount}>
+            +
+          </button>
+          </div>
+          <div>
+            <p>Total amount: {this.state.allTickets}</p>
+          <button id="confirm">Confirm</button>
+          </div>
+          <div className={styles.grid}></div>
+          <div class="navbar" id="myNavbar">
+            <Link href={"/qr"}>QR</Link>
+            <Link href={"/coupons"}>Coupons</Link>
+            <Link href={"/refunds"}>Refunds</Link>
+            <Link href={"/pictures"}>Pics</Link>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
+export default Page;
+/* var stripe_load = () => {
+  if (process.browser) {
+      var aScript = document.createElement('script');
+      aScript.type = 'text/javascript';
+      aScript.src = " https://js.stripe.com/v3/";
+
+      document.head.appendChild(aScript);
+      aScript.onload = () => {
+
+      };
+  }
+}; */
