fc821198f64bf385c7c0b2f916aa1a8dca1306c8
Nightfly <nightfly2013@gmail.com>
2021-04-23 14:01:20 +0200

    Initial commit for firebase testing branch
    Co-authored-by: Sebski123 <sebthomle@gmail.com>

--- the_good_haastival_website/pages/index.js
+++ the_good_haastival_website/pages/index.js
@@ -1,28 +1,16 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Haastival App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Page Title</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to The *SlightlyMediocre* Haastival website!
-        </h1>
-        <div className={styles.grid}>
-        </div>
-        <div class="navbar" id="myNavbar">
-          <Link href={'/qr'}>QR</Link>
-          <Link href={'/coupons'}>Coupons</Link>
-          <Link href={'/refunds'}>Refunds</Link>
-          <Link href={'/pictures'}>Pics</Link>
-        </div>
+      <main >
+        <div id="root"></div>
+        <script src="./firbasetest.js"></script>
       </main>
     </div>
-  )
+  );
 }
