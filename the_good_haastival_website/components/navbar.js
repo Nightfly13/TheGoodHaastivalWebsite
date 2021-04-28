a8a273a5446151cb71425e1b3649d6f3e5097db5
Andrej Kralovic <andr14b7@edu.ucl.dk>
2021-04-28 14:51:01 +0200

    make coupons page not clickable

--- the_good_haastival_website/components/navbar.js
+++ the_good_haastival_website/components/navbar.js
@@ -4,9 +4,8 @@ import { Component } from "react"
 class Navbar extends Component{
     render(){
         return(
-            <div class="navbar" id="myNavbar">
+            <div className="navbar" id="myNavbar">
             <Link href={'/qr'}>QR</Link>
-            <Link href={'/coupons'}>Coupons</Link>
             <Link href={'/refunds'}>Refunds</Link>
             <Link href={'/pictures'}>Pics</Link>
             <Link href={'/pictures'}>Logout</Link>
