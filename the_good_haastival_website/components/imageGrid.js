62d66708ca95ad1d0f7d2333e0c5d11724c689a2
Andrej Kralovic <andr14b7@edu.ucl.dk>
2021-05-03 14:35:53 +0200

    implement expand on click

--- the_good_haastival_website/components/imageGrid.js
+++ the_good_haastival_website/components/imageGrid.js
@@ -2,12 +2,17 @@ import "firebase/storage";
 import firebase from "firebase/app";
 import { firebaseConfig } from "../config";
 import React from "react";
-
+import imgStyling from "./imageGrid.module.css"
+const isBrowser = typeof window != "undefined";
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 } else {
   firebase.app(); // if already initialized, use that one
+}
+
+if (isBrowser) {
+  
 }
 
 class ImageGrid extends React.Component {
@@ -41,7 +46,29 @@ class ImageGrid extends React.Component {
 
   render() {
     const imageTags = this.state.images.map((elem, idx) => (
-        <img key={idx} src={elem} style={{width:32 + "vw", height:33+"vw"}}></img>
+      <div key={"outerDiv"+idx.toString()} style={{display:"inline"}}>
+          <img key={idx} src={elem} style={{width:32 + "vw", height:33+"vw"}} id={"mainImg"+idx.toString()} className={imgStyling.myImg} onClick={
+            function()
+            {
+              var modal = document.getElementById("myModal"+idx);
+              var captionText = document.getElementById("caption"+idx);
+              var modalImg = document.getElementById("imgNr"+idx);
+              var mainImg = document.getElementById("mainImg"+idx);
+              modal.style.display = "block";
+              modalImg.src = mainImg.src;
+            }
+          }/>
+          <div id={"myModal"+idx.toString()} className={imgStyling.modal}>
+            <span className={imgStyling.close} onClick={
+              function()
+              {
+                var modal = document.getElementById("myModal"+idx);
+                modal.style.display = "none";
+              }
+            }>x</span>
+            <img id={"imgNr"+idx.toString()} className={imgStyling.modalContent}/>
+          </div>
+      </div>
     ));
     return <div>{imageTags}</div>;
   }
