950686a51a094782bca0f071a128b3016b366a3b
sebski123 <sebthomle@gmail.com>
2021-05-05 18:24:29 +0200

    Added download and delete buttons to the pictures page

--- the_good_haastival_website/components/imageGrid.js
+++ the_good_haastival_website/components/imageGrid.js
@@ -21,12 +21,84 @@ class ImageGrid extends React.Component {
   };
 
   handleDownload = (url, filename) => {
-    fetch(url, {
-      mode: "no-cors",
-      responseType: "blob",
-    }).then((res) => {
-      fileDownload(res.data, filename);
-    });
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = "blob";
+    xhr.onload = (event) => {
+      var blob = xhr.response;
+      var blobURL =
+        window.URL && window.URL.createObjectURL
+          ? window.URL.createObjectURL(blob)
+          : window.webkitURL.createObjectURL(blob);
+      var tempLink = document.createElement("a");
+      tempLink.style.display = "none";
+      tempLink.href = blobURL;
+      tempLink.setAttribute("download", filename);
+
+      // Safari thinks _blank anchor are pop ups. We only want to set _blank
+      // target if the browser does not support the HTML5 download attribute.
+      // This allows you to download files in desktop safari if pop up blocking
+      // is enabled.
+      if (typeof tempLink.download === "undefined") {
+        tempLink.setAttribute("target", "_blank");
+      }
+
+      document.body.appendChild(tempLink);
+      tempLink.click();
+
+      // Fixes "webkit blob resource error 1"
+      setTimeout(function () {
+        document.body.removeChild(tempLink);
+        window.URL.revokeObjectURL(blobURL);
+      }, 200);
+    };
+    xhr.open("GET", url);
+    xhr.send();
+  };
+
+  createUndoButton = (timeOutTimer) => {
+    let currentButton = document.getElementById("undoButton");
+
+    let button = document.createElement("button");
+    button.id = "undoButton";
+    button.style.position = "fixed";
+    button.style.top = "20px";
+    button.style.left = "35%";
+    button.style.border = "none";
+    button.style.backgroundColor = "yellow";
+    button.style.color = "red";
+    //button.style.opacity = "0.5";
+    //button.style.right = "0"
+    button.style.width = "30%";
+    button.style.height = "30px";
+    button.style.zIndex = "2";
+    button.innerText = "Click to undo previous delete";
+
+    button.onclick = () => {
+      clearTimeout(timeOutTimer);
+      let currentButton = document.getElementById("undoButton");
+      if (currentButton.parentNode.contains(currentButton)) {
+        button.parentNode.removeChild(currentButton);
+      }
+    };
+
+    if (currentButton) {
+      currentButton.replaceWith(button);
+    } else {
+      document.body.appendChild(button);
+    }
+  };
+
+  moveFile = (url, destination) => {
+    let imgFileName = url.match(/verify%2F(.+)\?alt/)[1];
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = "blob";
+    xhr.onload = (event) => {
+      var blob = xhr.response;
+      firebase.storage().ref().child(destination).child(imgFileName).put(blob);
+      firebase.storage().refFromURL(url).delete();
+    };
+    xhr.open("GET", url);
+    xhr.send();
   };
 
   getImages() {
@@ -52,6 +124,16 @@ class ImageGrid extends React.Component {
     this.getImages();
   }
 
+  showModal = (idx) => {
+    var modal = document.getElementById("myModal" + idx);
+    var modalImg = document.getElementById("imgNr" + idx);
+    var mainImg = document.getElementById("mainImg" + idx);
+    if (modal) {
+      modal.style.display = "block";
+      modalImg.src = mainImg.src;
+    }
+  };
+
   render() {
     const imageTags = this.state.images.map((elem, idx) => (
       <div key={"outerDiv" + idx.toString()} style={{ display: "inline" }}>
@@ -61,14 +143,7 @@ class ImageGrid extends React.Component {
           style={{ width: 32 + "vw", height: 33 + "vw" }}
           id={"mainImg" + idx.toString()}
           className={imgStyling.myImg}
-          onClick={function () {
-            var modal = document.getElementById("myModal" + idx);
-            var captionText = document.getElementById("caption" + idx);
-            var modalImg = document.getElementById("imgNr" + idx);
-            var mainImg = document.getElementById("mainImg" + idx);
-            modal.style.display = "block";
-            modalImg.src = mainImg.src;
-          }}
+          onClick={() => this.showModal(idx)}
         />
         <div id={"myModal" + idx.toString()} className={imgStyling.modal}>
           <span
@@ -85,13 +160,35 @@ class ImageGrid extends React.Component {
             className={imgStyling.modalContent}
           />
           <div className={imgStyling.buttonDiv}>
-            <button className={`${imgStyling.button} ${imgStyling.leftButton}`}>
+            <button
+              className={`${imgStyling.button} ${imgStyling.leftButton}`}
+              onClick={() => {
+                var currentModal = document.getElementById("myModal" + idx);
+                var currentMainImg = document.getElementById("mainImg" + idx);
+
+                currentModal.style.display = "none";
+
+                let timeOut = setTimeout(() => {
+                  currentMainImg.style.display = "none";
+                  let currentButton = document.getElementById("undoButton");
+                  if (currentButton) {
+                    currentButton.parentNode.removeChild(currentButton);
+                  }
+                  firebase.storage().refFromURL(elem).delete();
+                }, 3000);
+
+                this.createUndoButton(timeOut);
+                this.showModal(idx + 1);
+              }}
+            >
               Delete
             </button>
             <button
               className={`${imgStyling.button} ${imgStyling.rightButton}`}
               onClick={() => {
-                this.handleDownload(elem, "test-download" + idx + ".jpg");
+                this.handleDownload(elem, "haastivalImg" + idx + ".jpg");
+                this.moveFile(elem, "verified-images");
+                this.showModal(idx + 1);
               }}
             >
               Download
