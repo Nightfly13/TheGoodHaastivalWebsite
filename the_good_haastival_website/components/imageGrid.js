b9705fb6a17bf284ea45828e4dd674143a333f51
sebski123 <sebthomle@gmail.com>
2021-05-05 19:42:11 +0200

    Now gets config from env variables

--- the_good_haastival_website/components/imageGrid.js
+++ the_good_haastival_website/components/imageGrid.js
@@ -1,13 +1,21 @@
 import "firebase/storage";
 import firebase from "firebase/app";
-import { firebaseConfig } from "../config";
 import React from "react";
 import imgStyling from "./imageGrid.module.css";
-import fileDownload from "js-file-download";
 const isBrowser = typeof window != "undefined";
 
+console.log(process.env.NEXT_PUBLIC_STORAGEBUCKET)
+
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp({
+    apiKey: process.env.NEXT_PUBLIC_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_DATABASEURL,
+    projectId: process.env.NEXT_PUBLIC_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+    appId: process.env.NEXT_PUBLIC_APPID,
+  });
 } else {
   firebase.app(); // if already initialized, use that one
 }
