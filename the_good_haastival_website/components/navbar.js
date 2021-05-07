952d252c1793a3ecfc78ef522327b2dc09de4f26
sebski123 <sebthomle@gmail.com>
2021-05-07 15:47:07 +0200

    Added logout functionallity to navbar

--- the_good_haastival_website/components/navbar.js
+++ the_good_haastival_website/components/navbar.js
@@ -1,17 +1,24 @@
-import Link from "next/link"
-import { Component } from "react"
+import Link from "next/link";
+import { Component } from "react";
 
-class Navbar extends Component{
-    render(){
-        return(
-            <div className="navbar" id="myNavbar">
-            <Link href={'/qr'}>QR</Link>
-            <Link href={'/refunds'}>Refunds</Link>
-            <Link href={'/pictures'}>Pics</Link>
-            <Link href={'/pictures'}>Logout</Link>
-          </div>
-        )
-    }
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar" id="myNavbar">
+        <Link href={"/qr"}>QR</Link>
+        <Link href={"/refunds"}>Refunds</Link>
+        <Link href={"/pictures"}>Pics</Link>
+        <a
+          onClick={() => {
+            document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+            window.location.href = "/login";
+          }}
+        >
+          Logout
+        </a>
+      </div>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
