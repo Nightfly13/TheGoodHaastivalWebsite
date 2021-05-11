6a3edcda6d5aef4cb3aec66bd7217d7dbccaf3ef
sebski123 <sebthomle@gmail.com>
2021-05-11 13:52:29 +0200

    Added missing dependency

--- the_good_haastival_website/pages/refunds.js
+++ the_good_haastival_website/pages/refunds.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Navbar from '../components/navbar.js';
+import checkIfTokenIsValid from "../lib/checkToken";
 import styles from '../styles/Home.module.css';
 const isBrowser = typeof window != "undefined";
 
@@ -17,7 +18,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-        PENIS PENIS PENIS PENIS
+        Refunds
         </h1>
         <div className={styles.grid}>
         </div>
