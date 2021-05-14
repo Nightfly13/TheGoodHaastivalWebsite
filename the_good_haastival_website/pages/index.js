aea8419b3614bc72a840ca99232662fc61b3c0e3
sebski123 <sebthomle@gmail.com>
2021-05-14 13:39:34 +0200

    Added explanations of what the different pages do

--- the_good_haastival_website/pages/index.js
+++ the_good_haastival_website/pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head';
-import Navbar from '../components/navbar.js';
+import Head from "next/head";
+import Navbar from "../components/navbar.js";
 import * as checkToken from "../lib/checkToken";
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
 
 const isBrowser = typeof window != "undefined";
 
@@ -22,11 +22,17 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to The *SlightlyMediocre* Haastival website!
+          Welcome to the Haastival Companion app!
         </h1>
-        <div className={styles.grid}>
-        </div>
-        <Navbar/>
+        <h2 style={{color: "green"}}>Your have logged in successfully!</h2>
+        <h2>Pages</h2>
+        <h3>QR</h3>
+        <p>The QR page enables you to scan codes of festival attendees and indicate how many tickets they have used to buy your delicious food. And obviuosly, we are keeping track of that for you!</p>
+        <h3 style={{marginBottom: "0"}}>Pics</h3>
+        <p style={{color: "red", fontSize: "10px", marginTop: "0"}}>Admin only, which basically means that if you are not Maria, you can't view them, sorry.</p>
+        <p>Pictures page contains images from festival atendees, which can be view, deleted or downloaded and used on Insta hopefully.</p>
+        <div className={styles.grid}></div>
+        <Navbar />
       </main>
     </div>
   );
